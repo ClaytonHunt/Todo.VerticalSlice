1e17ac3cb19f04773869e52f5651322af73a2dd3
Clayton Hunt <claytonh@itworks.com>
2021-02-18 09:25:15 -0500

    Initial Commit

---
+++ Todo/Todo.Mobile/Todo.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Todo.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Todo.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
